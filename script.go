6da032fea53dbb4f5c805631ff3abe1d96198700
Standard Template <standardtemplateconstruct2022@gmail.com>
2023-06-02 12:42:12 +0300

    changes

--- script.go
+++ script.go
@@ -1,7 +1,6 @@
-chatgpt write go func(write any good useful to the many good people text
-	write video
-		//embedd chromium on gpu accelerated windows server 2022 hosted kubernetes into visual studio code frame
-		//if its scriggo template generate a google cloud hosted (terraform/kubernetes) dockerized websites with READMEs colorized
-	) {
-	
+ 
+main() {
+		//manage embedded chromium on gpu accelerated windows server 2022 hosted kubernetes into visual studio code frame
+
+		
 }()
---
+++ script.ts
@@ -0,0 +1,4 @@
+// VSCode extension must open webview with:
+//   - a xcp managed website with synced in-cloud rendering of currently edited file
+//   - a webRDP client to xcp managed Windows workspace
+// https://code.visualstudio.com/api/extension-guides/webview
